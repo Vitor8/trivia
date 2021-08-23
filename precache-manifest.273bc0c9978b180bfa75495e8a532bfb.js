c57ff157df335701d404d625d62e9c0f6581707b
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-08-22 22:00:39 -0300

    Updates

---
+++ precache-manifest.273bc0c9978b180bfa75495e8a532bfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41af272ff004e6d8a2e37f18aeabe215",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "710249d201c106cc0103",
+    "url": "/trivia/static/css/main.01af38dc.chunk.css"
+  },
+  {
+    "revision": "3af922cf643e549ea285",
+    "url": "/trivia/static/js/2.f3a2b0e0.chunk.js"
+  },
+  {
+    "revision": "764370226b334fc0e6779014159c5cb1",
+    "url": "/trivia/static/js/2.f3a2b0e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "710249d201c106cc0103",
+    "url": "/trivia/static/js/main.7571ff1f.chunk.js"
+  },
+  {
+    "revision": "8cf3ddda99dc054ff6df",
+    "url": "/trivia/static/js/runtime-main.55dca74b.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
